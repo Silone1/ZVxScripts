0574bd96c4f7b997ce4b34e04c3ca407c0862096
ArchZombie0x <archzombielord@gmail.com>
2013-07-16 17:51:52 -0400

    Update /info

--- js_modules/info_command.js
+++ js_modules/info_command.js
@@ -26,6 +26,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
     info:
     {
+        server: true
+        ,
         desc: "Shows information about users"
         ,
         perm: function (src)
@@ -40,8 +42,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
             {
                 var test = this.profile.profileByName(cmd.args[x]);
                 if (test == -1) test = this.profile.profileByIP(cmd.args[x]);
-                
-                if (test == -1) 
+
+                if (test == -1)
                 {
                     this.com.message([src], "Can't find user " + cmd.args[x], this.theme.WARN);
                     continue;
@@ -56,14 +58,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
                     this.script.log(this.profile.profileByIP(cmd.args[x]));
                     continue;
                 }
-                
+
                 profids.push(test);
             }
 
             if (profids.length == 0) return;
-            
+
             var m = [];
-            for (var x in profids)
+            for ( x in profids)
             {
                 try {
                     m.push("<b>Info about profile #" + this.text.escapeHTML(""+profids[x]) + ":</b>");
@@ -77,7 +79,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
                 m.push("Last used name: " + this.text.escapeHTML(""+this.profile.database.profiles[profids[x]].lastName));
                 m.push("Last used IP: " + this.text.escapeHTML(""+this.profile.database.profiles[profids[x]].lastIP));
                 m.push("Names: " + this.text.escapeHTML(""+JSON.stringify(this.profile.profileNames(profids[x]))));
-                m.push("IP Addresses: " + this.text.escapeHTML(""+JSON.stringify(this.profile.profileIPs(profids[x]))));                       
+                m.push("IP Addresses: " + this.text.escapeHTML(""+JSON.stringify(this.profile.profileIPs(profids[x]))));
             }
 
             this.com.message([src], "<br/>"+m.join("<br/>"), this.theme.INFO, true);
