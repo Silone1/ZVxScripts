e1395b8315b3eb1bb29dd3fb7c7c70739e42214b
ArchZombie0x <archzombielord@gmail.com>
2013-07-23 21:52:06 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 if ! test -d "$1"
 then
-    exec echo Provide a directory to install to, e.g.: ./install.sh ../pokemon-online
+    exec echo Provide a directory to install to, e.g.: ./install.sh ../pokemon-online/bin
 else
     install_to=$1
     echo Installing to $install_to
-    cp -f `pwd`/scripts.js ${install_to}/bin
-    cp -f `pwd`/COPYING ${install_to}/bin/ZSCRIPTS_COPYING
-    if test -h ${install_to}/bin/js_modules
+    cp -f `pwd`/scripts.js ${install_to}
+    cp -f `pwd`/COPYING ${install_to}/ZSCRIPTS_COPYING
+    if test -h ${install_to}/js_modules
     then
-        rm ${install_to}/bin/js_modules
+        rm ${install_to}/js_modules
     else
-        if test -d ${install_to}/bin/js_modules
+        if test -d ${install_to}/js_modules
         then
             echo wtf error
             exit
         fi
     fi
-    ln -s `pwd`/js_modules ${install_to}/bin/js_modules
-    if ! test -d ${install_to}/bin/js_databases
+    ln -s `pwd`/js_modules ${install_to}/js_modules
+    if ! test -d ${install_to}/js_databases
     then
-        mkdir ${install_to}/bin/js_databases
+        mkdir ${install_to}/js_databases
         
     fi
     echo NOTE Symbolic linked js_modules
