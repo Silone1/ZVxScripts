2d29c4e00f42b8d97e706eba1d05eabf8a9535d6
ArchZombie0x <archzombielord@gmail.com>
2013-07-27 14:47:16 -0400

    Fix bug in mutelist.

--- js_modules/mute_command.js
+++ js_modules/mute_command.js
@@ -182,7 +182,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
         server:true,
         perm: function (src)
         {
-            return sys.auth(src) >= 2;
+            return true;
         }
         ,
         code: function (src)
