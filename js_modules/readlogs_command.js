b06ba23f642f7dbad08e7fd3d4e3219618c27d3f
ArchZombie0x <archzombielord@gmail.com>
2013-07-31 16:00:48 -0400

    Make readlogs able to be used by the server.

--- js_modules/readlogs_command.js
+++ js_modules/readlogs_command.js
@@ -24,6 +24,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     ,
     readlogs:
     {
+        server: true,
+        
         options:
         {
             count: "How many entries to read.",
