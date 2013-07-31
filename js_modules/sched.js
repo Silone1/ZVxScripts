1d249ec4a79bbeda7ae0768f16703df25a41e755
ArchZombie0x <archzombielord@gmail.com>
2013-07-31 15:48:05 -0400

    Update Scheduler

--- js_modules/sched.js
+++ js_modules/sched.js
@@ -1,68 +1,76 @@
 /*  ///////////////////////// LEGAL NOTICE ///////////////////////////////
 
-This file is part of ZScripts,
-a modular script framework for Pokemon Online server scripting.
+ This file is part of ZScripts,
+ a modular script framework for Pokemon Online server scripting.
 
-Copyright (C) 2013  Ryan P. Nicholl, aka "ArchZombie" / "ArchZombie0x", <archzombielord@gmail.com>
+ Copyright (C) 2013  Ryan P. Nicholl, aka "ArchZombie" / "ArchZombie0x", <archzombielord@gmail.com>
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-/////////////////////// END LEGAL NOTICE /////////////////////////////// */
+ /////////////////////// END LEGAL NOTICE /////////////////////////////// */
 ({
-    timers: []
-    ,
-    needsSorting: false
-    ,
-    i: 0
-    ,
-    at: function (time, callback)
-    {
-        this.timers.push({time: time, callback: callback, id: this.i++});
-        this.needsSorting = true;
-    }
-    ,
-    step: function (callback)
-    {
-        if (this.needsSorting)
-        {   
-            this.timers.sort( function (a, b) { return a.time - b.time } );
-            this.needsSorting = false;
-        }
 
-        var now = +new Date;
+     timers: null,
+     timerDB: null,
+     needsSorting: false,
+     i: 0,
+     
+     
+     setTimer: function (module, time, callback)
+     {
+         this.i++;
+         
+         this.timerDB[i] = []; 
+     },
+     
 
-        for (var i = 0; i < this.timers.length; i++)
-        {
-            if (this.timers[i].time <= now)
-            {
-                this.timers[i].callback();
-            }
-            else
-            {
-                break;
-            }
-        }
+     at: function (time, callback)
+     {
+         this.timers.push({time: time, callback: callback, id: this.i++});
+         this.needsSorting = true;
+     },
 
-        this.timers.splice(0, i);
-    }
-    ,
-    loadModule: function ()
-    {
-        // var lock = this;
+     
+     step: function (callback)
+     {
+         if (this.needsSorting)
+         {   
+             this.timers.sort( function (a, b) { return a.time - b.time } );
+             this.needsSorting = false;
+         }
 
-        //sys.setTimer(function() {lock.tick();}, 200, true);
+         var now = +new Date;
+         
+         for (var i = 0; i < this.timers.length; i++)
+         {
+             if (this.timers[i].time <= now)
+             {
+                 this.timers[i].callback();
+             }
+             else
+             {
+                 break;
+             }
+         }
+         
+         this.timers.splice(0, i);
+     },
 
-        this.script.registerHandler("step", this);
-    }
-});
+
+     loadModule: function ()
+     {
+         this.script.registerHandler("step", this);
+     }
+
+ });
