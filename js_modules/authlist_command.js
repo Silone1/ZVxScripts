c1892224706db3ed780c3f5735098dca35e699fc
ArchZombie0x <archzombielord@gmail.com>
2013-08-18 13:01:24 -0400

    Authlist updatess.
    Closes issue #8.

--- js_modules/authlist_command.js
+++ js_modules/authlist_command.js
@@ -1,60 +1,79 @@
 /*  ///////////////////////// LEGAL NOTICE ///////////////////////////////
 
-This file is part of ZScripts,
-a modular script framework for Pokemon Online server scripting.
+ This file is part of ZScripts,
+ a modular script framework for Pokemon Online server scripting.
 
-Copyright (C) 2013  Ryan P. Nicholl, aka "ArchZombie" / "ArchZombie0x", <archzombielord@gmail.com>
+ Copyright (C) 2013  Ryan P. Nicholl, aka "ArchZombie" / "ArchZombie0x", <archzombielord@gmail.com>
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-/////////////////////// END LEGAL NOTICE /////////////////////////////// */
+ /////////////////////// END LEGAL NOTICE /////////////////////////////// */
 ({
 
-    require:
-    [
-        "commands"
-        ,
-        "com"
-        ,
-        "theme"
-        ,
-        "text"
-    ]
-    ,
-    loadModule: function ()
-    {
-        this.commands.registerCommand("authlist", this);
-    }
-    ,
-    authlist: 
-    {
-        desc: "Lists the auth on the server."
-        ,
-        aliases: ["auths"]
-        ,
-        perm: function () { return true; }
-        ,
-        code: function (src) 
-        {
-            this.com.message([src], "Authlist:", this.theme.INFO);
-            var auths = sys.dbAuths();
-            
-            for (var x in auths)
-            {
-                this.com.message([src], "<b>" + ([null, "Moderator", "Admin", "Owner"][sys.dbAuth(auths[x])] || "?") + "</b> " + this.text.escapeHTML(auths[x]) +
-                                 (sys.id(auths[x])?" <span style='color:green'>online</span>":" <span style='color:red'>offline</span>"), this.theme.RAW, true);                
-            }
-        }    
-    }   
-});
+     require: ["commands", "text", "com", "theme", "util", "user"],
+
+     LEVELS: null,
+
+     LEVEL_ORDER: null,
+
+     loadModule: function ()
+     {
+         this.commands.registerCommand("authlist", this);
+
+         this.LEVELS = this.util.keyify([1, 2, 3]);
+         this.LEVEL_ORDER = this.util.keyify([3,2,1,0]);
+     },
+
+     authlist: 
+     {
+         desc: "Lists the auth on the server."
+         ,
+         aliases: ["auths"]
+         ,
+         perm: function () { return true; }
+         ,
+         code: function (src) 
+         {
+             this.com.message([src], "Authlist:", this.theme.INFO);
+             var auths = sys.dbAuths();
+
+             var lvs = [[],[],[],[]];
+
+             for (var x in auths)
+             {
+                 var lv = sys.dbAuth(x);
+                 
+                 if (lv in this.LEVELS) lvs[lv].push(auths[x]);
+              
+                 else lvs[0].push(auths[x]);
+                 
+             }
+
+             lvs.forEach(
+                 function (e) 
+                 {
+                     e.sort();
+                 }
+             );
+              
+             for (var y in this.LEVEL_ORDER) for (x in lvs[y])
+             {
+                 print(x, y);
+                 this.com.message(src, "<b>" + ([null, "Moderator", "Admin", "Owner"][sys.dbAuth(lvs[y][x])] || "?") + "</b> " + this.text.escapeHTML(this.user.nameToProper(lvs[y][x])) +
+                                  (sys.id(lvs[y][x])?" <span style='color:green'>online ("+sys.id(lvs[y][x])+")</span>":" <span style='color:red'>offline</span>"), this.theme.RAW, true);                
+             }
+
+         }    
+     }   
+ });
