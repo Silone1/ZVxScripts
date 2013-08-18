00dbc19fff3c589899b104b6035c6e77f97b9337
ArchZombie0x <archzombielord@gmail.com>
2013-08-18 08:47:53 -0400

    Less changes

--- js_modules/less.js
+++ js_modules/less.js
@@ -1,24 +1,24 @@
 /*  ///////////////////////// LEGAL NOTICE ///////////////////////////////
 
-This file is part of ZScripts,
-a modular script framework for Pokemon Online server scripting.
+ This file is part of ZScripts,
+ a modular script framework for Pokemon Online server scripting.
 
-Copyright (C) 2013  Ryan P. Nicholl, aka "ArchZombie" / "ArchZombie0x", <archzombielord@gmail.com>
+ Copyright (C) 2013  Ryan P. Nicholl, aka "ArchZombie" / "ArchZombie0x", <archzombielord@gmail.com>
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-/////////////////////// END LEGAL NOTICE /////////////////////////////// */
+ /////////////////////// END LEGAL NOTICE /////////////////////////////// */
 /** Provides the less function
  * @name less
  * @memberOf script.modules
@@ -26,68 +26,73 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * */
 /** @scope script.modules.less */
 ({
-    require: ["chat", "com", "theme"]
-    ,
-    /** Less function
-     * @param {Number} src User to send less to
-     * @param msg The message to send
-     * @param html If msg is html.
-     */
-    less: function (src, msg, html)
-    {
-        var lines;
+     require: ["chat", "com", "theme"],
 
-        if (src == 0) {
-            this.com.message(src, msg, this.theme.INFO, html); // for the moment this is not supported with server :(
-            return;
-        }
+     LESS_LENGTH: 30,
 
-        if (!html) lines = msg.split(/\n/g);
-        else lines = msg.split(/<\s*br\s*\/?>/g);
+     /** Less function
+      * @param {Number} src User to send less to
+      * @param msg The message to send
+      * @param html If msg is html.
+      */
+     less: function (src, msg, html)
+     {
+         var lines;
 
-        var p = 0;
+         if (!html) lines = msg.split(/\n/g);
+         else lines = msg.split(/<\s*br\s*\/?>/g);
 
-        var bind = this;
+         print(lines.length);
 
-        function showPage()
-        {
+         if (src == 0 || this.LESS_LENGTH >= lines.length) {
+             this.com.message(src, msg, -1, html); 
+             return;
+         }
 
-            for (var i = 0; i < 20 && i+20*p < lines.length; i++)
-            {
-                bind.com.message([src], lines[i+20*p], -1, html);
-            }
-            p++;
+         var p = 0;
 
-            if (p*20 >= lines.length)
-            {
-                bind.com.message([src], "<span style='color:white;background-color:black'>END OF TEXT. EXITED VIEW MODE.</span>", -1, true);
-                return;
-            }
+         var bind = this;
 
-            bind.com.message([src], "<span style='color:white;background-color:black'>END OF PAGE " + (p) +". TYPE 'NEXT' TO GO FORWARD, 'EXIT' TO QUIT.</span>", -1, true);
+         function showPage()
+         {
 
-            bind.chat.registerCapture(src, handle, bind);
+             for (var i = 0; ((i < bind.LESS_LENGTH) && (i+(bind.LESS_LENGTH*p) < lines.length)); i++)
+             {
+                 bind.com.message(src, lines[i+bind.LESS_LENGTH*p], -1, html);
+             }
 
-        }
+             p++;
 
-        function handle (_, msg)
-        {
-            if (msg.toLowerCase() == "exit")
-            {
-                bind.com.message([src], "<span style='color:white;background-color:black'>EXITED VIEW MODE.</span>", -1, true);
-                return;
-            }
+             if (p*bind.LESS_LENGTH >= lines.length)
+             {
+                 bind.com.message([src], "<span style='color:white;background-color:black'>END OF TEXT. EXITED VIEW MODE.</span>", -1, true);
+                 return;
+             }
+             
+             bind.com.message([src], "<span style='color:white;background-color:black'>END OF PAGE " + (p) +" OF "+ Math.ceil(lines.length / bind.LESS_LENGTH) + ". TYPE 'NEXT' TO GO FORWARD, 'EXIT' TO QUIT.</span>", -1, true);
 
-            if (msg.toLowerCase() == "next" || msg.toLowerCase() == "n")
-            {
-                showPage();
-                return;
-            }
+             bind.chat.registerCapture(src, handle, bind);
 
-            bind.com.message([src], "<span style='color:white;background-color:black'>YOU ARE IN VIEW MODE. TYPE 'NEXT' TO GO TO THE NEXT PAGE, OR 'EXIT' TO EXIT VIEW MODE.</span>", -1, true);
-            bind.chat.registerCapture(src, handle, bind);
-        }
+         }
 
-        showPage();
-    }
-});
+         function handle (_, msg)
+         {
+             if (msg.toLowerCase() == "exit")
+             {
+                 bind.com.message([src], "<span style='color:white;background-color:black'>EXITED VIEW MODE.</span>", -1, true);
+                 return;
+             }
+
+             if (msg.toLowerCase() == "next" || msg.toLowerCase() == "n")
+             {
+                 showPage();
+                 return;
+             }
+
+             bind.com.message([src], "<span style='color:white;background-color:black'>YOU ARE IN VIEW MODE. TYPE 'NEXT' TO GO TO THE NEXT PAGE, OR 'EXIT' TO EXIT VIEW MODE.</span>", -1, true);
+             bind.chat.registerCapture(src, handle, bind);
+         }
+
+         showPage(this);
+     }
+ });
