5d74431f00e496db720bf2a1c33dac2e06ed5fb7
ArchZombie0x <archzombielord@gmail.com>
2013-08-18 13:11:09 -0400

    Remove unessecary plus.

--- js_modules/setauth_command.js
+++ js_modules/setauth_command.js
@@ -85,7 +85,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
                     (
                         ((sys.id(cmd.args[x]) ? sys.auth(sys.id(cmd.args[x])) : sys.dbAuth(cmd.args[x])) >= sys.auth(src))
                         ||
-                        (+level >= sys.auth(src))
+                        (level >= sys.auth(src))
                     )
                 )
                 {
