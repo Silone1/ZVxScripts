813150586afa9e89cfd52f595215b05484522a16
ArchZombie0x <archzombielord@gmail.com>
2013-08-27 22:46:16 -0400

    Add user group support

--- js_modules/automute.js
+++ js_modules/automute.js
@@ -1,5 +1,26 @@
+/*  ///////////////////////// LEGAL NOTICE ///////////////////////////////
+
+ This file is part of ZScripts,
+ a modular script framework for Pokemon Online server scripting.
+
+ Copyright (C) 2013  Ryan P. Nicholl, aka "ArchZombie" / "ArchZombie0x", <archzombielord@gmail.com>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ /////////////////////// END LEGAL NOTICE /////////////////////////////// */
 ({
-     require: ["chat", "theme", "com"],
+     require: ["chat", "theme", "com", "user"],
 
      counter:null,
 
@@ -14,7 +35,7 @@
 
      autoFilter: function (src, msg)
      {
-         if (sys.auth(src) >= 2) return msg;
+         if ("SERVEROP" in this.user.groups(src)) return msg;
          var ip = sys.ip(src);
 
          if (! (ip in this.counter)) this.counter[ip] = 0;
