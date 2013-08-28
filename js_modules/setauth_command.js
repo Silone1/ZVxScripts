1cb90e0aeb11fe6ebef76b2b6d41f08f9952e186
ArchZombie0x <archzombielord@gmail.com>
2013-08-27 22:54:35 -0400

    Security on setauth

--- js_modules/setauth_command.js
+++ js_modules/setauth_command.js
@@ -37,7 +37,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
         ,
         perm: function (src)
         {
-            return sys.auth(src) >= 2;
+            return "AUTHOP" in this.user.groups(src);
         }
         ,
         code: function (src, cmd, chan)
