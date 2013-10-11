2ce8f40fad5c6b4b732f55f128cc4984cfb80c3e
ArchZombie0x <archzombielord@gmail.com>
2013-10-11 01:07:30 -0400

    Bugfix /unmute :(

--- js_modules/mute_command.js
+++ js_modules/mute_command.js
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /////////////////////// END LEGAL NOTICE /////////////////////////////// */
 ({
-    require: ["commands", "security",  "text", "com", "theme", "time", "user"]
+    require: ["commands", "security",  "text", "com", "theme", "time", "user", "util"]
     ,
     unmuteall:
     {
