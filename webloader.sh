34a362d47e4b65bd2c6588d5dc9c318c5bac0757
ArchZombie0x <archzombielord@gmail.com>
2013-10-20 20:44:48 -0400

    Add webloader file

---
+++ webloader.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git log | grep 'commit' -m 1 | nodejs -e "process.stdout.write(require('fs').readFileSync('/dev/stdin').toString().replace(/^commit (.+)[\r\n]+$/,'\$1'));" > ./bin/commit-stable
+
+echo -n / > bin/slash
+echo -n https://raw.github.com/ArchZombie/ZVxScripts/ > bin/basepath
+cat bin/basepath bin/commit-stable bin/slash > bin/baseurl
+git ls-files js_modules/* > bin/filelist
+
+git commit bin -m 'Webloader version update'
+git push 
