6b30ac7462cadc8e07f019a6947964f3cd2be002
ArchZombie0x <archzombielord@gmail.com>
2013-10-26 01:57:57 -0400

    webscript

--- webloader.sh
+++ webloader.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-git log | grep 'commit' -m 1 | nodejs -e "process.stdout.write(require('fs').readFileSync('/dev/stdin').toString().replace(/^commit (.+)[\r\n]+$/,'\$1'));" > ./bin/commit-stable
+#git log | grep 'commit' -m 1 | nodejs -e "process.stdout.write(require('fs').readFileSync('/dev/stdin').toString().replace(/^commit (.+)[\r\n]+$/,'\$1'));" > ./bin/commit-stable
 
-echo -n / > bin/slash
-echo -n https://raw.github.com/ArchZombie/ZVxScripts/ > bin/basepath
-cat bin/basepath bin/commit-stable bin/slash > bin/baseurl
-git ls-files js_modules/* > bin/filelist
+#echo -n / > bin/slash
+#echo -n https://raw.github.com/ArchZombie/ZVxScripts/ > bin/basepath
+#cat bin/basepath bin/commit-stable bin/slash > bin/baseurl
+#git ls-files js_modules/* > bin/filelist
 
 cd tools
 nodejs packager.js
 cd ../
 
-git commit bin -m 'Webloader version update'
-git push 
+git commit bin -m 'Package version update'
+git push
